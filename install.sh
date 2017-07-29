14637d5bc7a7703e86322777af5b63ba9279accd
Andrew Stevanus <hoot893@gmail.com>
2017-07-29 19:00:07 -0400

    Replaced installer customization with use of environment variables
    As little configuration as possible should be done in the installer, since it
    makes it makes this program more difficult to package properly. It should also
    be possible to use the program checked out directly from git without running the
    installer. Configuration can instead be done with flags and environment
    variables, and potentially a config file in the future. Also, don't hardcode eog
    as the default previewer; just use xdg-open instead. The user's XDG defaults
    should be respected.

--- install.sh
+++ install.sh
@@ -9,28 +9,11 @@ command -v zopflipng >/dev/null 2>&1 && echo "Found: zopflipng" || { echo >&2 "E
 command -v /opt/mozjpeg/bin/cjpeg >/dev/null 2>&1 && echo "Found: mozjpeg" || { echo >&2 "ERROR: Missing Dependency 'cjpeg'. Make sure it's in '/opt/mozjpeg/bin/'. Aborting!"; exit 1; }
 command -v ffmpeg >/dev/null 2>&1 && echo "Found: ffmpeg" || { echo >&2 "WARNING: Missing Optional Dependency 'ffmpeg'."; }
 
-echo ""
-read -p "Name: [Leave empty for 'screenshot'] " name
-if [[ $name == "" ]]; then name="screenshot"; fi
-echo -e "Installing As:\n\n"$name"\n"
-
-read -p "Image Viewer for Preview: [Leave empty for 'eog'] " defviewer
-if [[ $defviewer == "" ]]; then defviewer=eog; fi
-echo -e "Using:\n\n"$defviewer"\n"
-
-read -p "Default Encoder (png or jpeg): [Leave empty for 'jpeg'] " encoder
-if [[ $encoder == "" ]]; then encoder="jpeg"; fi
-echo -e "Using:\n\n"$encoder"\n"
-
 read -p ":: Proceed with installation? [Y/n] " yn
 if [[ $yn == "y" ]] || [[ $yn == "Y" ]]; then
 
-    sed -i "s/__PROGNAME__/"$name"/g" ./$name
-    sed -i "s/__DEFAULTVIEWER__/"$defviewer"/g" ./$name
-    sed -i "s/__ENCODER__/"$encoder"/g" ./$name
-
-    chmod +x ./$name
-    sudo mv ./$name /usr/bin/$name
+    chmod +x ./screenshot
+    sudo mv ./screenshot /usr/bin/screenshot
 
     # Fake Progress Bar
     tleng=$(($(tput cols) - 25))
